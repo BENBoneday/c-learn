5c862ae358b7974c81a79c3988fcba0b7a17e5bf
BENBoneday <85631569+BENBoneday@users.noreply.github.com>
2021-10-01 22:08:30 +0800

    动态内存版通讯录

---
+++ newmain.c
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"newcontact.h"
+
+void menu()
+{
+	printf("****** 1.添加联系人     2.删除联系人\n");
+	printf("****** 3.搜索联系人     4.修改联系人\n");
+	printf("****** 5.展示联系人     6.排序 *****\n");
+	printf("****** 0.退出                  *****\n");
+}
+int main()
+{
+	menu();
+	int input=0;
+	struct  contact con;
+	Initcontact(&con);
+	do
+	{
+		printf("请输入选择的功能\n");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case Exit:
+			DestroyContact(&con);
+			break;
+		case add:
+			addcontact(&con);
+			break;
+		case delete:
+			deletecontact(&con);
+			break;
+		case search:
+			searchcontact(&con);
+			break;
+		case modify:
+			modifycontact(&con);
+			break;
+		case show:
+			showcontact(&con);
+			break;
+		case sort:
+			sortcontact(&con);
+			break;
+		}
+	} while (input);
+	
+	
+	return 0;
+}
